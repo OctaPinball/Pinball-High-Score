68984e260fabc2e6992c4f603d60eb0614e5d9dc
OctaPinball <scholtz.balint@gmail.com>
2022-09-25 19:56:33 +0200

    [2] Mass Delete & Log in complete

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.static('static'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
+/*
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 app.use('/tasks',function(req, res, next){
   console.log('hello /tasks');
   });
@@ -24,3 +24,4 @@ app.use('/tasks',function(req, res, next){
   app.post('/',function(req, res, next){
   console.log('hello / with POST');
   });
+*/
